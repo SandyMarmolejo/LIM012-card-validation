2ea1d3a5897788730f694e2e821b3184bcad9939
Estudiante <sandymarmolejoc@gmail.com>
2020-02-20 10:16:57 -0500

    Primer avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+import validator from './validator.js';
+ 
+console.log(validator);
+ 
+//Evento boton validar
+ 
+const btnValidar = document.getElementById("btnValidar");
+ 
+btnValidar.addEventListener("click"), () => {
+    document.getElementById("divResultadoValido").className = "mostrarDiv"
+    document.getElementById("divRegresar").className = "mostrarDiv"
+    document.getElementById("divInicio").className = "ocultarDiv"
+    document.getElementById("divIngreseNumeroTarjeta").className = "ocultarDiv"
+    document.getElementById("divSeleccioneTarjeta").className = "ocultarDiv"
+}
+ 
+ 
+//Evento boton regresar
+const btnRegresar = document.getElementById("btnRegresar");
+ 
+btnRegresar.addEventListener("click"), () => {
+    document.getElementById("divResultadoValido").className = "ocultarDiv"
+    document.getElementById("divRegresar").className = "ocultarDiv"
+    document.getElementById("divInicio").className = "mostrarDiv"
+    document.getElementById("divIngreseNumeroTarjeta").className = "mostrarDiv"
+    document.getElementById("divSeleccioneTarjeta").className = "mostrarDiv"
+}
--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,43 @@ const validator = {
 };
 
 export default validator;
+
+const validator = {
+  // ...
+};
+ 
+export default validator;
+ 
+const numeroTarjeta = "4556364607935616";
+ 
+const reverse = function (array) {
+ 
+  const newArray = [];
+ 
+  const size = array.length;
+ 
+  const lastPosition = size -1;
+ 
+  for (let i = lastPosition; i>=0; i--){
+    newArray.push(array[i]);
+  }
+  
+  //Solo si ocupa posicion impar se le multiplica x 2 al numero, los demas se mantienen
+ 
+    for (i= 0; i <= lastPosition; i = i + 1 ) {
+      if (i % 2 === 1){
+        newArray[i] = (newArray[i]*2).toString();
+      }
+    }
+ 
+//Sumar las cifras de los que son mayor igual a 10
+    A[0]
+      for(i=0; i <= lastPosition; i = i + 1){
+          if (i.toString() >=10){
+          newArray[0] = parseInt (newArray.toString()[0]) + parseInt (newArray.toString()[1]);
+         }
+      }
+ 
+ 
+return newArray;
+}
