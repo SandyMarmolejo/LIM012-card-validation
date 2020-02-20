93cc7e9630cf92a82404a27eadf47264bca4b72e
Estudiante <sandymarmolejoc@gmail.com>
2020-02-20 13:03:15 -0500

    Avance del Algoritmo

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import validator from './validator.js';
 
 console.log(validator);
-
-import validator from './validator.js';
- 
-console.log(validator);
  
 //Evento boton validar
  
 const btnValidar = document.getElementById("btnValidar");
  
-btnValidar.addEventListener("click"), () => {
-    document.getElementById("divResultadoValido").className = "mostrarDiv"
-    document.getElementById("divRegresar").className = "mostrarDiv"
+btnValidar.addEventListener("click", () => {
+
     document.getElementById("divInicio").className = "ocultarDiv"
     document.getElementById("divIngreseNumeroTarjeta").className = "ocultarDiv"
     document.getElementById("divSeleccioneTarjeta").className = "ocultarDiv"
-}
- 
+    document.getElementById("divResultadoValido").className = "mostrarDiv"
+    document.getElementById("divRegresar").className = "mostrarDiv"
+
+});
  
 //Evento boton regresar
 const btnRegresar = document.getElementById("btnRegresar");
  
-btnRegresar.addEventListener("click"), () => {
+btnRegresar.addEventListener("click", () => {
     document.getElementById("divResultadoValido").className = "ocultarDiv"
     document.getElementById("divRegresar").className = "ocultarDiv"
     document.getElementById("divInicio").className = "mostrarDiv"
     document.getElementById("divIngreseNumeroTarjeta").className = "mostrarDiv"
     document.getElementById("divSeleccioneTarjeta").className = "mostrarDiv"
-}
+});
+
--- src/validator.js
+++ src/validator.js
@@ -4,42 +4,41 @@ const validator = {
 
 export default validator;
 
-const validator = {
-  // ...
-};
- 
-export default validator;
  
 const numeroTarjeta = "4556364607935616";
- 
 const reverse = function (array) {
- 
-  const newArray = [];
- 
-  const size = array.length;
- 
-  const lastPosition = size -1;
- 
-  for (let i = lastPosition; i>=0; i--){
-    newArray.push(array[i]);
-  }
-  
+ const newArray = [];
+ const size = array.length;
+ const lastPosition = size -1;
+ for (let i = lastPosition; i>=0; i--){
+   newArray.push(array[i]);
+ }
   //Solo si ocupa posicion impar se le multiplica x 2 al numero, los demas se mantienen
+let suma = 0;
+let tarjeta = 0;
+   for (i= 0; i <= lastPosition; i = i + 1 ) {
+       if (i % 2 === 1){
+       newArray[i] = (newArray[i]*2).toString();
+       }
  
-    for (i= 0; i <= lastPosition; i = i + 1 ) {
-      if (i % 2 === 1){
-        newArray[i] = (newArray[i]*2).toString();
+       if (parseInt(newArray[i])>=10) {
+           cifraUno=parseInt(newArray[i][0])
+           cifraDos=parseInt(newArray[i][1])
+           newArray[i]= cifraUno + cifraDos;
+       }
+ 
+       newArray[i] = parseInt(newArray[i]);
+ 
+       suma = suma + newArray[i];
+   }
+ 
+ 
+   if (suma%10==0){
+        tarjeta=true;
       }
-    }
- 
-//Sumar las cifras de los que son mayor igual a 10
-    A[0]
-      for(i=0; i <= lastPosition; i = i + 1){
-          if (i.toString() >=10){
-          newArray[0] = parseInt (newArray.toString()[0]) + parseInt (newArray.toString()[1]);
-         }
+      else {
+        tarjeta= false;
       }
  
- 
-return newArray;
+return tarjeta;
 }
